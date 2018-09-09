f22b8f04530f0108e8e085cbc0f22d79a80f37f3
gaya902008 <42521132+gaya902008@users.noreply.github.com>
2018-09-09 16:58:15 +0300

    modules & controllers

--- FlyMe/Models/Airport.cs
+++ FlyMe/Models/Airport.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace FlyMe.Models
 {
     public class Airport
     {
+        [Key]
         public int ID { get; set; }
 
         public string Country { get; set; }
