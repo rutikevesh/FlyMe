3f5777335fd170616dbae563fa0a3648dbe266b7
rutikevesh <42501102+rutikevesh@users.noreply.github.com>
2018-10-12 16:31:14 +0300

    Added Statistics to project

---
+++ FlyMe/Controllers/StatisticsController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FlyMe.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using FlyMe.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlyMe.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        private readonly FlyMeContext _context;
+
+        public StatisticsController(FlyMeContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Stats()
+        {
+            var mostSoldFlights = _context.Ticket.Include(ticket => ticket.Flight)
+                                                .ThenInclude(Flight => Flight.Airplane)
+                                             .Include(ticket => ticket.Flight)
+                                                .ThenInclude(Flight => Flight.SourceAirport)
+                                             .Include(ticket => ticket.Flight)
+                                                .ThenInclude(Flight => Flight.DestAirport)
+                                                 .Where(ticket => (ticket.Buyer != null))
+                                                .GroupBy(ticket => ticket.Flight)
+                                                .Select(o => new SoldFlightsView
+                                                {
+                                                    Flight = o.Key,
+                                                    TicketsSold = o.Count()
+                                                })
+                                                .OrderByDescending(o => o.TicketsSold)
+                                                .ToList();
+            var mostVisitedAirports = (from acronyms in _context.Airport.Select(airport => airport.Acronyms)
+                                       select new CountryVisitorsAmount
+                                       {
+                                           AirportAcronims = acronyms,
+                                           numberOfVisitors = 0
+                                       }).ToList();
+
+            foreach (var flight in mostSoldFlights)
+            {
+                foreach (var airport in mostVisitedAirports)
+                {
+                    if (flight.Flight.DestAirport.Acronyms == airport.AirportAcronims)
+                        airport.numberOfVisitors += flight.TicketsSold;
+                }
+            }
+            return View(mostVisitedAirports);
+        }
+
+    }
+}
---
+++ FlyMe/Models/CountryVisitorsAmount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlyMe.Models
+{
+    public class CountryVisitorsAmount
+    {
+        public int numberOfVisitors { get; set; }
+
+        public string AirportAcronims { get; set; }
+    }
+}
